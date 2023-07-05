9f35c0cee5602c8d45f8bc875de2c3995eb47a40
Tracy Sonyah <sonyahtracy@gmail.com>
2023-07-05 00:11:57 +0000

    making progress

--- script.js
+++ script.js
@@ -3,23 +3,31 @@ let minutes = 0;
 let seconds = 0;
 let milliseconds = 0;
 let timerDisplay = document.getElementById('timer-display');
+let isRunning = false;
 
 // implement the below funtion using the setInteral funtion
 function startTimer() {
+    if(isRunning) {
+        return;
+    }
+    isRunning = true;
     timeInterval = setInterval(() => {
         if(milliseconds === 0) {
-            seconds ++;
-            console.log('me')
-        }  else if(seconds === 60) {
-            minutes ++;
-            console.log('u')
+            if(seconds === 60) {
+                minutes ++;
+                seconds = 0;
+            } else{
+                seconds ++;
+            }
+            milliseconds = 0;
+        }   
+        else {
+            milliseconds += 100;
         }
-
-        milliseconds += 100;
 
         timerDisplay.textContent = formatTime(minutes, seconds, milliseconds);
 
-    }, 1000)
+    }, 100)
 }
 
 function formatTime(minutes, seconds, milliseconds) {
@@ -33,14 +41,30 @@ function formatTime(minutes, seconds, milliseconds) {
 }
 
 function pauseTimer() {
-    console.log("HH")
+    if(!isRunning) {
+        return;
+    }
+    isRunning = false;
     clearInterval(timeInterval);
 }
 
-function resetTimer() {
-    clearInterval(timeInterval);
+function stopTimer() {
+    pauseTimer();
     minutes = 0;
     seconds = 0;
     milliseconds = 0;
-    timerDisplay.textContent = '00:00:00';
+    timerDisplay.textContent = formatTime(minutes, seconds, milliseconds);
 }
+
+
+function resetTimer() {
+    stopTimer();
+    startTimer();
+    // clearInterval(timeInterval);
+    // minutes = 0;
+    // seconds = 0;
+    // milliseconds = 0;
+    // timerDisplay.textContent = '00:00:00';
+}
+
+document.getElementById
